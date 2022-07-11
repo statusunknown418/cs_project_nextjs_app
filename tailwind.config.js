ebac0bd3b1f8a79c47b34ee7497c06fbab77970d
alvaro <alvarodevcode@outlook.com>
2022-07-11 17:12:53 -0500

    style: updated config and styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
       fontFamily: {
         raleway: "Raleway, system-ui, sans-serif",
+        mono: ["IBM Plex Mono", ...defaultTheme.fontFamily.mono],
       },
     },
   },
